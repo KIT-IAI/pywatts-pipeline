ec954f029913f3793734ed5048f6bb57f2e9c7cb
benHeid <benHeid@users.noreply.github.com>
2022-12-06 16:34:10 +0100

    Enable the input selection without sampler and clockshift that often confuses users.

--- pywatts_pipeline/core/steps/result_step.py
+++ pywatts_pipeline/core/steps/result_step.py
@@ -14,28 +14,16 @@ class ResultStep(BaseStep):
     def __init__(self, input_steps, buffer_element: str):
         super().__init__(input_steps=input_steps)
         self.buffer_element = buffer_element
-        # TODO should have own buffer
 
     def get_result(
-        self, start: pd.Timestamp, return_all=False, minimum_data=(0, pd.Timedelta(0))
+            self, start: pd.Timestamp, return_all=False, minimum_data=(0, pd.Timedelta(0))
     ):
         """
         Returns the specified result of the previous step.
         """
-        # TODO fill own buffer with results of previous steps
-
-        # TODO use _pack_data
-        # TODO can we get rid off get_all?
-        if not return_all:
-            result = list(self.input_steps.values())[0].get_result(
-                start, self.buffer_element, minimum_data=minimum_data
-            )
-        else:
-            result = {
-                self.buffer_element: list(self.input_steps.values())[0].get_result(
-                    start, self.buffer_element, minimum_data=minimum_data
-                )
-            }
+        result = list(self.input_steps.values())[0].get_result(
+            start, True, minimum_data=minimum_data
+        )
         if result is None or self.buffer_element not in result.keys():
             return None
         return result[self.buffer_element]
--- pywatts_pipeline/core/steps/step_information.py
+++ pywatts_pipeline/core/steps/step_information.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 from pywatts_pipeline.core.steps.base_step import BaseStep
+from pywatts_pipeline.core.steps.selection_step import SelectionStep
 
 
 class StepInformation:
@@ -13,13 +16,19 @@ class StepInformation:
         self.step = step
         self.pipeline = pipeline
 
-    def __getitem__(self, item: str):
+    def __getitem__(self, item: Union[str, int, slice]):
         from pywatts_pipeline.core.steps.step import Step
 
-        if isinstance(self.step, Step):
-            result_step = self.step.get_result_step(item)
-            self.pipeline.add(module=result_step, input_ids=[self.step.id])
-            return StepInformation(result_step, self.pipeline)
+        if isinstance(item, str):
+            if isinstance(self.step, Step):
+                result_step = self.step.get_result_step(item)
+                self.pipeline.add(module=result_step, input_ids=[self.step.id])
+                return StepInformation(result_step, self.pipeline)
+        elif isinstance(item, int) or isinstance(item, slice) or isinstance(item, list):
+            selection_step = SelectionStep({"step": self.step}, item)
+            self.pipeline.add(module=selection_step, input_ids=[self.step.id])
+            return StepInformation(selection_step, self.pipeline)
+
         return self
 
 
